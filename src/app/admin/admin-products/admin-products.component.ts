cb9987596ca00e87983570b7b994b4bf4694eee8
Atul Alatkar <atalatk@microsoft.com>
2018-01-12 15:49:45 -0800

    Delete and search

--- src/app/admin/admin-products/admin-products.component.ts
+++ src/app/admin/admin-products/admin-products.component.ts
@@ -1,19 +1,33 @@
 import { ProductService } from './../../services/product.service';
 import { Component, OnInit } from '@angular/core';
+import { OnDestroy } from '@angular/core/src/metadata/lifecycle_hooks';
+import { Subscription } from 'rxjs/Subscription';
 
 @Component({
   selector: 'app-admin-products',
   templateUrl: './admin-products.component.html',
   styleUrls: ['./admin-products.component.css']
 })
-export class AdminProductsComponent implements OnInit {
-  products$;
+export class AdminProductsComponent implements OnInit, OnDestroy {
+  // products$;
+  products: {title: string}[];
+  filteredProducts: any[];
+  subscription: Subscription;
 
   constructor(private productsService: ProductService) {
-    this.products$ = this.productsService.getAll();
+    this.subscription = this.productsService.getAll().subscribe(products => this.filteredProducts = this.products = products);
+  }
+
+  filter(query: string) {
+    console.log(query);
+    this.filteredProducts = (query) ? this.products.filter(p => p.title.toLowerCase().includes(query.toLowerCase())) : this.products;
   }
 
   ngOnInit() {
+  }
+
+  ngOnDestroy(): void {
+    this.subscription.unsubscribe();
   }
 
 }
