3f2642a7946868659350787643cce69b42c0335a
Atul Alatkar <alatkar@yahoo.com>
2018-01-27 19:36:03 -0800

    Section 341 complete

--- src/app/models/shopping-cart-item.ts
+++ src/app/models/shopping-cart-item.ts
@@ -1,8 +1,14 @@
 import { Product } from './product';
 
 export class ShoppingCartItem {
-    product: Product;
-    quantity: number;
+    //product: Product;
+    //quantity: number;
+    get totalPrice() {
+      return this.product.price * this.quantity;
+    }
+    constructor(public product: Product, public quantity: number) {
+
+    }
     /*
   $key: string;
   title: string;
